d8cc49a83e3be7dc4c94fcccdb7286fa42841e3f
Marta <159387209+martaic@users.noreply.github.com>
2025-03-26 09:07:56 +0000

    generador_cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,55 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const palos = ['♦', '♥', '♠', '♣'];
+const valores = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+const randomMaker = (arr) => Math.floor(Math.random() * arr.length);
+const valueExtract = arr => arr[randomMaker(arr)];
+
+let tiempoRestante = 10;
+
+const cardCreation = (arrPalos, arrValores = []) => {
+  const palo = valueExtract(arrPalos);
+  const valor = valueExtract(arrValores);
+
+  const paloUp = document.getElementById("paloUp");
+  const numero = document.getElementById("numero");
+  const paloDown = document.getElementById("paloDown");
+  const tiempoDisplay = document.getElementById("tiempo");
+
+  paloUp.textContent = palo;
+  numero.textContent = valor;
+  paloDown.textContent = palo;
+
+  if (palo === '♥' || palo === '♦') {
+    paloUp.classList.add("rojo");
+    paloDown.classList.add("rojo");
+    numero.classList.add("rojo");
+  } else {
+    paloUp.classList.add("negro");
+    paloDown.classList.add("negro");
+    numero.classList.add("negro");
+  }
+
+  tiempoRestante = 10;
 };
+
+document.getElementById("generateCardButton").addEventListener("click", function() {
+  cardCreation(palos, valores);
+});
+
+window.onload = function() {
+  cardCreation(palos, valores);
+
+  setInterval(() => {
+    tiempoRestante--;
+    document.getElementById("tiempo").textContent = `Nueva carta en: ${tiempoRestante}s`;
+
+    if (tiempoRestante === 0) {
+      cardCreation(palos, valores);
+    }
+  }, 1000);
+};
+
+
